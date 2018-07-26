7c9650fd774b50c65057431fddfd10a0cb3266a9
YuliaNorkina <yulia.norkina2@gmail.com>
2018-07-26 12:49:54 +0300

    complete implicit call

--- dev1/src/main/java/com/example/dev1/MainActivity.java
+++ dev1/src/main/java/com/example/dev1/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.dev1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String ACTION = "com.example.dev1.SHOW_PICTURE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button bShow = findViewById(R.id.bShow);
+        bShow.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(ACTION);
+        startActivity(intent);
     }
 }
