549a48cc538b45daf2ff771ce8c15cff1c95de45
YuliaNorkina <yulia.norkina2@gmail.com>
2018-07-26 13:46:06 +0300

    complete task

--- savedata/src/main/java/com/example/savedata/MainActivity.java
+++ savedata/src/main/java/com/example/savedata/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.savedata;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String COUNTER = "counter";
+    private static final String TAG = "myLogs";
+    private TextView tvCounter;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvCounter = findViewById(R.id.tvCounter);
+        Button bCounter = findViewById(R.id.bCounter);
+
+        bCounter.setOnClickListener(this);
+
+        if (savedInstanceState!=null){
+            count = savedInstanceState.getInt(COUNTER);
+        }
     }
+
+    @Override
+    public void onClick(View v) {
+        tvCounter.setText(String.valueOf(++count));
+        Log.d(TAG, "onClick counter = " + count);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(COUNTER, count);
+    }
+
 }
+
