faf2367f51ce74f34dc8f1af81a413a1374a0595
YuliaNorkina <yulia.norkina2@gmail.com>
2018-08-01 10:55:51 +0300

    done

--- nativecamera/src/main/java/com/example/nativecamera/MainActivity.java
+++ nativecamera/src/main/java/com/example/nativecamera/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.nativecamera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +22,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.bCamera:
-
+                Intent intent = new Intent(this, CameraActivity.class);
+                startActivity(intent);
                 break;
             case R.id.bOther:
-
+                Intent intent1 = new Intent(this, OtherActivity.class);
+                startActivity(intent1);
                 break;
         }
 
--- nativecamera/src/main/java/com/example/nativecamera/OtherActivity.java
+++ nativecamera/src/main/java/com/example/nativecamera/OtherActivity.java
@@ -1,13 +1,54 @@
 package com.example.nativecamera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class OtherActivity extends AppCompatActivity {
+public class OtherActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String EMAIL_ADDRESS = "yulia.norkina2@gmail.com";
+    public final String SUBJECT = "subject";
+    private final String LETTERS_TEXT = "This is new letter from Julia Norkina";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.other);
+
+        Button bEmail = findViewById(R.id.bEmail);
+        Button bShare = findViewById(R.id.bShare);
+        Button bPhone = findViewById(R.id.bPhone);
+
+       setAllOnClickListener(this, bEmail, bPhone, bShare);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bEmail:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_EMAIL, EMAIL_ADDRESS);
+                intent.putExtra(Intent.EXTRA_SUBJECT, SUBJECT);
+                intent.putExtra(Intent.EXTRA_TEXT, LETTERS_TEXT);
+                intent.setType("text/plain");
+                startActivity(Intent.createChooser(intent,"Choose App:"));
+                break;
+            case R.id.bShare:
+                Intent intent2 = new Intent(Intent.ACTION_SEND);
+                intent2.setType("text/plain");
+                startActivity(Intent.createChooser(intent2,"Choose App:"));
+                break;
+            case R.id.bPhone:
+                Intent intent1 = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent1);
+        }
+    }
+
+    public static void setAllOnClickListener(View.OnClickListener onClickListener, View... views) {
+        for (View view : views) {
+            view.setOnClickListener(onClickListener);
+        }
     }
 }
