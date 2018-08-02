8d644a1cfa7c18cb8a54044cccf5bf688de2a4e6
YuliaNorkina <yulia.norkina2@gmail.com>
2018-08-02 12:27:31 +0300

    fix some mistake

--- savedata/src/main/java/com/example/savedata/MainActivity.java
+++ savedata/src/main/java/com/example/savedata/MainActivity.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    public static final String COUNTER = "counter";
+    private static final String COUNTER = "counter";
     private static final String TAG = "myLogs";
     private TextView tvCounter;
     private int count = 0;
@@ -20,19 +19,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         tvCounter = findViewById(R.id.tvCounter);
-        Button bCounter = findViewById(R.id.bCounter);
-
-        bCounter.setOnClickListener(this);
-
-        if (savedInstanceState!=null){
+        findViewById(R.id.bCounter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvCounter.setText(String.valueOf(++count));
+                Log.d(TAG, "onClick counter = " + count);
+            }
+        });
+        if (savedInstanceState != null) {
             count = savedInstanceState.getInt(COUNTER);
         }
-    }
-
-    @Override
-    public void onClick(View v) {
-        tvCounter.setText(String.valueOf(++count));
-        Log.d(TAG, "onClick counter = " + count);
     }
 
     @Override
@@ -40,6 +36,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onSaveInstanceState(outState);
         outState.putInt(COUNTER, count);
     }
-
 }
 
