9168bb769b580df1e6e9e8047b464712ba3c4aa8
YuliaNorkina <yulia.norkina2@gmail.com>
2018-08-02 12:55:44 +0300

    fix some mistake

--- dev1/src/main/java/com/example/dev1/MainActivity.java
+++ dev1/src/main/java/com/example/dev1/MainActivity.java
@@ -4,24 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    public static final String ACTION = "com.example.dev1.SHOW_PICTURE";
+    private static final String ACTION = "com.example.dev1.SHOW_PICTURE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button bShow = findViewById(R.id.bShow);
-        bShow.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(ACTION);
-        startActivity(intent);
+        findViewById(R.id.bShow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ACTION);
+                startActivity(intent);
+            }
+        });
     }
 }
