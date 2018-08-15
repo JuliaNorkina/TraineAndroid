86bf7d8ba78fedc14aec68b05da5ce17bc362575
YuliaNorkina <yulia.norkina2@gmail.com>
2018-08-15 11:00:13 +0300

    complete, but if all fragments remove, button back dont work

--- checkboxwithfragment/src/main/java/com/example/checkboxwithfragment/MainActivity.java
+++ checkboxwithfragment/src/main/java/com/example/checkboxwithfragment/MainActivity.java
@@ -1,58 +1,76 @@
 package com.example.checkboxwithfragment;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    CheckBox cbRed;
+    CheckBox cbGreen;
+    CheckBox cbBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CheckBox cbRed = findViewById(R.id.cbRed);
-        CheckBox cbGreen = findViewById(R.id.cbGreen);
-        CheckBox cbBlue = findViewById(R.id.cbBlue);
+        cbRed = findViewById(R.id.cbRed);
+        cbGreen = findViewById(R.id.cbGreen);
+        cbBlue = findViewById(R.id.cbBlue);
+
         cbRed.setOnCheckedChangeListener(this);
         cbGreen.setOnCheckedChangeListener(this);
         cbBlue.setOnCheckedChangeListener(this);
-        Log.d("log", "onCreate");
+    }
 
+    @Override
+    public void onBackPressed() {
+        List<Fragment> fragmentById = getSupportFragmentManager().getFragments();
+        if (!fragmentById.isEmpty()) {
+            switch (fragmentById.get(0).getTag()) {
+                case "red":
+                    cbRed.setChecked(false);
+                    break;
+                case "green":
+                    cbGreen.setChecked(false);
+                    break;
+                case "blue":
+                    cbBlue.setChecked(false);
+                    break;
+            }
+        } else
+            super.onBackPressed();
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         switch (buttonView.getId()) {
             case R.id.cbRed:
-                if (isChecked) {
-                    transaction.add(R.id.flForFragment, MyFragment.newInstance(ContextCompat.getColor(this, R.color.red)), "red");
-
-                } else {
-                    transaction.remove(getFragmentManager().findFragmentByTag("red"));
-                }
+                manageFragment(isChecked, transaction, R.color.red, "red");
                 break;
             case R.id.cbGreen:
-                if (isChecked) {
-                    transaction.add(R.id.flForFragment, MyFragment.newInstance(ContextCompat.getColor(this, R.color.green)), "green");
-                } else {
-                    transaction.remove(getFragmentManager().findFragmentByTag("green"));
-                }
+                manageFragment(isChecked, transaction, R.color.green, "green");
                 break;
             case R.id.cbBlue:
-                if (isChecked) {
-                    transaction.add(R.id.flForFragment, MyFragment.newInstance(ContextCompat.getColor(this, R.color.blue)), "blue");
-                } else {
-                    transaction.remove(getFragmentManager().findFragmentByTag("blue"));
-                }
+                manageFragment(isChecked, transaction, R.color.blue, "blue");
                 break;
         }
-        transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    private void manageFragment(boolean isChecked, FragmentTransaction transaction, int color, String tag) {
+        if (isChecked) {
+            transaction.add(R.id.flForFragment, MyFragment.newInstance(ContextCompat.getColor(this, color)), tag);
+            transaction.addToBackStack(tag);
+        } else {
+            transaction.remove(getSupportFragmentManager().findFragmentByTag(tag));
+        }
     }
 }
--- checkboxwithfragment/src/main/java/com/example/checkboxwithfragment/MyFragment.java
+++ checkboxwithfragment/src/main/java/com/example/checkboxwithfragment/MyFragment.java
@@ -1,22 +1,25 @@
 package com.example.checkboxwithfragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MyFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.color_fragment, null);
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.setBackgroundColor(getArguments().getInt("color"));
+        if (getArguments() != null) {
+            view.setBackgroundColor(getArguments().getInt("color"));
+        }
     }
 
     public static MyFragment newInstance(int color) {
