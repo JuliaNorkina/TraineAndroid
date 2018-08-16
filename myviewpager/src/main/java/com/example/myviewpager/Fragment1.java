40821dde2b6b10e8b21b72752ef84d956f3d2f59
YuliaNorkina <yulia.norkina2@gmail.com>
2018-08-16 16:35:44 +0300

    complete

---
+++ myviewpager/src/main/java/com/example/myviewpager/Fragment1.java
@@ -0,0 +1,15 @@
+package com.example.myviewpager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, null);
+    }
+}
---
+++ myviewpager/src/main/java/com/example/myviewpager/Fragment5.java
@@ -0,0 +1,15 @@
+package com.example.myviewpager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment5 extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment5, null);
+    }
+}
